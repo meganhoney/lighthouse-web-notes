d20cda6eb45fa1a0cf1245c88fc20fda357a35e7
Megan Honey <mjthoney@gmail.com>
2022-04-05 18:28:50 +0000

    Use NodeJS assert library

--- Week_3/Day_1/say_hello.test.js
+++ Week_3/Day_1/say_hello.test.js
@@ -1,3 +1,6 @@
+const assert = require('assert');
+// no path - this library is built into NodeJS
+
 // .test is a good way to indicate nature of the file
 // https://nodejs.org/api/modules.html
 // assign require function to a variable
@@ -5,4 +8,16 @@
 // set export values in original file
 // if nothing exported, ourFunctions is empty object
 const ourFunctions = require('./say_hello');
-console.log(ourFunctions);
+
+// assign sayHello function to a variable by accessing object method
+const sayHello = ourFunctions.sayHello;
+
+const actual = sayHello('Sneha');
+const expected = 'Hello there, Sneha!';
+
+//console.log(assert);
+
+// Strict will type match as well
+// no output if true
+// detailed report if not equal
+assert.strictEqual(actual, expected);
