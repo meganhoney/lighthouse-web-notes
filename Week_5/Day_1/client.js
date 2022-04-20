baa58258258a2954dabeb5d06d454e4602a52f77
Megan Honey <mjthoney@gmail.com>
2022-04-20 19:37:32 +0000

    Username fix

--- Week_5/Day_1/client.js
+++ Week_5/Day_1/client.js
@@ -2,8 +2,22 @@ const net = require('net');
 
 // 
 const config = {
+  //host: '8.tcp.ngrok.io',
+  //port: 14589
   host: 'localhost',
   port: 3000
 };
 
 const client = net.createConnection(config);
+
+client.setEncoding('utf-8');
+
+// Read data from server
+client.on('data', (data) => {
+  console.log(data);
+});
+
+// submitting information to server
+process.stdin.on('data', (data) => {
+  client.write(data);
+});
--- Week_5/Day_1/server.js
+++ Week_5/Day_1/server.js
@@ -4,8 +4,37 @@ const port = 3000;
 
 const server = net.createServer();
 
-server.on('connection', () => {
-  console.log('A client has connected');
+const connections = [];
+
+server.on('connection', (connection) => {
+  // console.log('A client has connected');
+
+  connection.setEncoding('utf-8');
+
+  connections.push(connection);
+
+  // listen for data writes from clients
+  connection.on('data', (data) => {
+    console.log('A client submitted: ' + data);
+
+    if (data.startsWith('name: ')) {
+      data = data + ' ';
+      const name = data.split(' ')[1];
+      console.log(name);
+      return connection.username = name;
+    }
+
+    for (const conn of connections) {
+      if (conn !== connection && !conn._writableState.finished) {
+        const name = typeof connection.username !== 'undefined' ? connection.username.trim() : 'Client';
+        conn.write(
+          name
+          + ' says: '
+          + data.trim())
+      }
+    }
+  });
+ 
 });
 
 // .listen is  method, takes port and a cb fn
