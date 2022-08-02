8594c1f79c6f37fa136123034651abc601d2de64
Megan Honey <mjthoney@gmail.com>
2022-08-02 19:29:53 +0000

    Add comments

--- Week_4/Day_1/events.js
+++ Week_4/Day_1/events.js
@@ -5,10 +5,10 @@ const paragraph = document.getElementById('myParagraph');
 
 // callbacks are used when you need to wait for something asynchronous to happen
 // events are asynchronous
+// e = event - common
 button.addEventListener('click', (e) => {
   console.log(e); // obtains event info
   console.log('The button was clicked!');
 
   paragraph.innerHTML = 'The button was clicked!!!';
 });
-
